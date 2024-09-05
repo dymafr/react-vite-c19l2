edc3cde9bcee6467d5f827d1ede5b3ce691ba613
quentin <quentin@dyma.fr>
2024-09-05 07:51:19 +0200

    fix: packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import styles from './App.module.scss';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      <div className="flex-fill">
+      <div className="flex-fill d-flex">
         <Suspense>
           <Outlet />
         </Suspense>
